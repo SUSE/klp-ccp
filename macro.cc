SUSE/klp-ccp

commit c06a22a7391fe2cf99c6197e2d770a576945008c

macro: implement support for GNU style varargs
As an extension, GNU cpp supports giving the varargs
parameter a name different than "__VA_ARGS__":

  #define a (b...)

will make the vaargs contents accessible through "b".

Implement this.

Signed-off-by: Nicolai Stange <nstange@suse.de>

macro.cc
@@ -402,9 +402,6 @@ macro::macro(const std::string &name,
     _func_like(func_like),
     _variadic(variadic)
 {
-  if (_variadic)
-    _arg_names.push_back("__VA_ARGS__");
-
   // Due to the restriction that a ## concatenated token must again
   // yield a valid preprocessing token, macro evaluation can fail and
   // thus yield an error. Hence macro arguments must not get macro
@@ -529,6 +526,7 @@ macro::parse_macro_definition(const pp_tokens::const_iterator begin,
 
       if (it->is_punctuator("...")) {
 	variadic = true;
+	arg_names.push_back("__VA_ARGS__");
 	it = skip_ws(it + 1);
       } else if (it->is_id()) {
 	if (it->get_value() == "__VA_ARGS__") {
@@ -547,6 +545,10 @@ macro::parse_macro_definition(const pp_tokens::const_iterator begin,
 
 	arg_names.push_back(it->get_value());
 	it = skip_ws(it + 1);
+	if (it->is_punctuator("...")) {
+	  variadic = true;
+	  it = skip_ws(it + 1);
+	}
       } else {
 	code_remark remark(code_remark::severity::fatal,
 			   "garbage in macro argument list",
@@ -578,7 +580,7 @@ macro::parse_macro_definition(const pp_tokens::const_iterator begin,
 
   return create(name, func_like, variadic, std::move(arg_names),
 		_normalize_repl(it, end, func_like, unique_arg_names,
-				variadic, remarks),
+				remarks),
 		file_range(range_start, range_end));
 }
 
@@ -609,7 +611,6 @@ pp_tokens macro::_normalize_repl(const pp_tokens::const_iterator begin,
 				 const pp_tokens::const_iterator end,
 				 const bool func_like,
 				 const std::set<std::string> &arg_names,
-				 const bool variadic,
 				 code_remarks &remarks)
 {
   if (begin == end)
@@ -667,8 +668,7 @@ pp_tokens macro::_normalize_repl(const pp_tokens::const_iterator begin,
 	++it_arg;
       assert(it_arg == end || !it_arg->is_ws());
       if (it_arg == end || !it_arg->is_id() ||
-	  (!(variadic && it_arg->get_value() == "__VA_ARGS__") &&
-	   !arg_names.count(it_arg->get_value()))) {
+	  !arg_names.count(it_arg->get_value())) {
 	code_remark remark(code_remark::severity::fatal,
 			   "# in func-like macro not followed by parameter",
 			   (end - 1)->get_file_range());

macro.hh
@@ -111,7 +111,6 @@ namespace suse
 				       const pp_tokens::const_iterator end,
 				       const bool func_like,
 				       const std::set<std::string> &arg_names,
-				       const bool variadic,
 				       code_remarks &remarks);
 
       pp_tokens::const_iterator

commit a3ac3bf2d2fe452b754d12082a326655988ea793

macro::parse_macro_definition(): report __VA_ARGS__ in set of arguments
_normalize_repl() checks whether any stringify operator's argument
is a macro parameter by looking it up in the set of parameters.

However, parse_macro_definition() fails to insert __VA_ARGS__ to this
set.

Fix this.

Fixes: c06a22a7391f ("macro: implement support for GNU style varargs")
Signed-off-by: Nicolai Stange <nstange@suse.de>

macro.cc
@@ -558,6 +558,7 @@ macro::parse_macro_definition(const pp_tokens::const_iterator begin,
       if (it->is_punctuator("...")) {
 	variadic = true;
 	arg_names.push_back("__VA_ARGS__");
+	unique_arg_names.insert("__VA_ARGS__");
 	it = skip_ws(it + 1);
       } else if (it->is_id()) {
 	if (it->get_value() == "__VA_ARGS__") {