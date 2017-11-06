59cbb745029b3bf51e25c55118a72fac29a4ea3e
Nicolai Stange <nstange@suse.de>
2017-11-06 13:31:10 +0100

    parser: add testsuite
    Dump part of gcc's gcc/testsuite/gcc.dg/ testsuite for testing
    the parser.
    
    In particular, copyrights apply.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

---
+++ arch/stddef-x86_64.h
@@ -0,0 +1,14 @@
+#define __SIZE_TYPE__ unsigned long
+#define __PTRDIFF_TYPE__ long
+#define __INTPTR_TYPE__ long
+#define __UINTPTR_TYPE__ unsigned long
+#define __INT32_TYPE__ int
+#define __UINT32_TYPE__ unsigned int
+#define __INT64_TYPE__ long
+#define __UINT64_TYPE__ unsigned long
+#define __CHAR16_TYPE__ short
+#define __CHAR32_TYPE__ int
+#define __WCHAR_TYPE__ int
+
+typedef __int128 __int128_t;
+typedef unsigned __int128 __uint128_t;
