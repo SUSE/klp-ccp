deb2974bbb4db0fbe6b89b13fa266c9dc046b43c
Nicolai Stange <nstange@suse.de>
2017-11-12 19:14:15 +0100

    arch/stddef-x86_64.h: add some #defines
    ... which are required by the to be added testcases from gcc's testsuite.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- arch/stddef-x86_64.h
+++ arch/stddef-x86_64.h
@@ -2,13 +2,26 @@
 #define __PTRDIFF_TYPE__ long
 #define __INTPTR_TYPE__ long
 #define __UINTPTR_TYPE__ unsigned long
+#define __INT8_TYPE__ char
+#define __UINT8_TYPE__ unsigned char
+#define __INT_LEAST8_TYPE__ char
+#define __UINT_LEAST8_TYPE__ unsigned char
+#define __INT16_TYPE__ short
+#define __UINT16_TYPE__ unsigned short
+#define __INT_LEAST16_TYPE__ short
+#define __UINT_LEAST16_TYPE__ unsigned short
 #define __INT32_TYPE__ int
 #define __UINT32_TYPE__ unsigned int
+#define __INT_LEAST32_TYPE__ int
+#define __UINT_LEAST32_TYPE__ unsigned int
 #define __INT64_TYPE__ long
 #define __UINT64_TYPE__ unsigned long
+#define __INT_LEAST64_TYPE__ long
+#define __UINT_LEAST64_TYPE__ unsigned long
 #define __CHAR16_TYPE__ short
 #define __CHAR32_TYPE__ int
 #define __WCHAR_TYPE__ int
+#define __WINT_TYPE__ long
 
 typedef __int128 __int128_t;
 typedef unsigned __int128 __uint128_t;
@@ -31,8 +44,21 @@ typedef unsigned __int128 __uint128_t;
 #define __LDBL_MIN__ 3.36210314311209350626e-4932L
 #define __LDBL_MAX__ 1.18973149535723176502e+4932L
 
+#define __FLT_MANT_DIG__ 24
+#define __DBL_MANT_DIG__ 53
+
+#define __SIZEOF_POINTER__ sizeof(void*)
+#define __SIZEOF_SIZE_T__ sizeof(unsigned long)
+#define __SIZEOF_PTRDIFF_T__ sizeof(long)
+#define __SIZEOF_SHORT__ sizeof(short)
 #define __SIZEOF_INT__ sizeof(int)
 #define __SIZEOF_LONG__ sizeof(long)
+#define __SIZEOF_LONG_LONG__ sizeof(long long)
+#define __SIZEOF_WCHAR_T__ sizeof(int)
+#define __SIZEOF_WINT_T__ sizeof(long)
+#define __SIZEOF_FLOAT__ sizeof(float)
+#define __SIZEOF_DOUBLE__ sizeof(double)
+#define __SIZEOF_LONG_DOUBLE__ sizeof(long double)
 
 
 #define NULL ((void *) 0)
