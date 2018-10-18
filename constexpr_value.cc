306f6c0960b9f0cb50d2b5f96bea4d0dbdf0b5ae
Nicolai Stange <nstange@suse.de>
2018-10-18 10:21:27 +0200

    Introduce constexpr_value for storing the values of constant expressions
    C99 has the notion of constant expressions which can be evaluated at
    compile time:
    - integer constant expressions,
    - arithmetic constant expression of either integer or float type,
    - address constants.
    
    In particular the first kind, integer constant expressions, can be used
    as part of type definitions: for example for specifying bitfield widths
    or array dimensions.
    
    Apart from the type system, these values will also be needed when it
    comes to implementing conditional inclusion.
    
    Provide the constexpr_value class for storing the kind and the actual
    value of expressions evaluating to constants.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

---
+++ constexpr_value.cc
@@ -0,0 +1,344 @@
+#include "constexpr_value.hh"
+#include "ast.hh"
+#include <cassert>
+
+using namespace suse::cp;
+using namespace suse::cp::ast;
+using namespace suse::cp::types;
+
+constexpr_value::address_constant::
+address_constant()
+  : _bk(base_kind::bk_constant), _offset(0), _dereferenced(false)
+{}
+
+constexpr_value::address_constant::
+address_constant(const target_int &base)
+  : _bk(base_kind::bk_constant), _offset(_target_int_to_limbs(base)),
+    _dereferenced(false)
+{}
+
+constexpr_value::address_constant::
+address_constant(const init_declarator &base, const bool dereferenced)
+  : _bk(base_kind::bk_init_declarator), _base_id(&base),
+    _dereferenced(dereferenced)
+{}
+
+constexpr_value::address_constant::
+address_constant(const function_definition &base, const bool dereferenced)
+  : _bk(base_kind::bk_function_definition), _base_fd(&base),
+    _dereferenced(dereferenced)
+{}
+
+constexpr_value::address_constant::
+address_constant(const stmt_labeled &base, const bool dereferenced)
+  : _bk(base_kind::bk_stmt_labeled), _base_stmt_labeled(&base),
+    _dereferenced(dereferenced)
+{}
+
+constexpr_value::address_constant::
+address_constant(const string_literal &base, const bool dereferenced)
+  : _bk(base_kind::bk_string_literal), _base_string_literal(&base),
+    _dereferenced(dereferenced)
+{}
+
+constexpr_value::address_constant::
+address_constant(const expr_compound_literal &base,
+		 const bool dereferenced)
+  : _bk(base_kind::bk_compound_literal), _base_compound_literal(&base),
+    _dereferenced(dereferenced)
+{}
+
+void constexpr_value::address_constant::undereference() noexcept
+{
+  assert(_dereferenced);
+  _dereferenced = false;
+}
+
+void constexpr_value::address_constant::dereference() noexcept
+{
+  assert(!_dereferenced);
+  _dereferenced = true;
+}
+
+void constexpr_value::address_constant::
+add_to_offset(mpa::limbs &&ls)
+{
+  // Take ls to be positive. If the sign bit is not zero,
+  // enlarge it.
+  if (ls.test_bit(ls.width() - 1)) {
+    ls.resize(mpa::limbs::width_to_size(ls.width() + 1));
+  }
+
+  _offset = _offset.add_signed(ls);
+}
+
+void constexpr_value::address_constant::
+add_to_offset(bool negate, const target_int &i, const mpa::limbs &fac)
+{
+  mpa::limbs add = i.get_limbs();
+  if (i.is_signed() && i.is_negative()) {
+    // Multiplicate positive numbers only.
+    negate = !negate;
+    add = add.complement();
+  }
+  add = add * fac;
+
+  // Make sure there's room for a sign bit.
+  if (add.test_bit(add.width() - 1)) {
+    add.resize(mpa::limbs::width_to_size(add.width()) + 1);
+  }
+
+  if (negate)
+    add = add.complement();
+
+  _offset = _offset.add_signed(add);
+}
+
+bool constexpr_value::address_constant::is_null() const noexcept
+{
+  return (_bk == base_kind::bk_constant && !_offset);
+}
+
+bool constexpr_value::address_constant::
+has_same_base_as(const address_constant &ac) const noexcept
+{
+  switch (_bk) {
+  case base_kind::bk_constant:
+    return ac._bk == base_kind::bk_constant;
+
+  case base_kind::bk_init_declarator:
+    if (ac._bk == base_kind::bk_init_declarator) {
+      return _base_id->get_linkage().is_linked_to(*ac._base_id);
+    } else if (ac._bk == base_kind::bk_function_definition) {
+      return _base_id->get_linkage().is_linked_to(*ac._base_fd);
+    } else {
+      return false;
+    }
+
+  case base_kind::bk_function_definition:
+    if (ac._bk == base_kind::bk_init_declarator) {
+      return _base_fd->get_linkage().is_linked_to(*ac._base_id);
+    } else if (ac._bk == base_kind::bk_function_definition) {
+      return _base_fd->get_linkage().is_linked_to(*ac._base_fd);
+    } else {
+      return false;
+    }
+
+  case base_kind::bk_stmt_labeled:
+    return (ac._bk == base_kind::bk_stmt_labeled &&
+	    _base_stmt_labeled == ac._base_stmt_labeled);
+
+  case base_kind::bk_string_literal:
+    return (ac._bk == base_kind::bk_string_literal &&
+	    _base_string_literal == ac._base_string_literal);
+
+  case base_kind::bk_compound_literal:
+    return (ac._bk == base_kind::bk_compound_literal &&
+	    _base_compound_literal == ac._base_compound_literal);
+  };
+}
+
+mpa::limbs constexpr_value::address_constant::
+_target_int_to_limbs(const target_int &ti)
+{
+  // Make sure there's a sign bit.
+  if (ti.is_signed() ||
+      ti.min_required_width() < ti.get_limbs().width())
+    return ti.get_limbs();
+
+  mpa::limbs ls = ti.get_limbs();
+  ls.resize(ls.size() + 1);
+  return ls;
+}
+
+constexpr_value::constexpr_value(const _constness_type &c, const target_int &ti)
+  : _value_kind(value_kind::vk_int), _ti(ti), _c(c)
+{}
+
+constexpr_value::constexpr_value(const _constness_type &c,
+				 const target_float &tf)
+  : _value_kind(value_kind::vk_float), _tf(tf), _c(c)
+{}
+
+constexpr_value::constexpr_value(const _constness_type &c,
+				 const address_constant &ac)
+  : _value_kind(value_kind::vk_address), _ac(ac), _c(c)
+{}
+
+constexpr_value::constexpr_value(const integer_constant_expr_tag c_tag,
+				 const target_int &ti)
+  : constexpr_value(_constness_from_tag(c_tag), ti)
+{}
+
+constexpr_value::constexpr_value(const arithmetic_constant_expr_tag c_tag,
+				 const target_int &ti)
+  : constexpr_value(_constness_from_tag(c_tag), ti)
+{}
+
+constexpr_value::constexpr_value(const target_int &ti)
+  : constexpr_value(_constness_from_tag(), ti)
+{}
+
+constexpr_value::constexpr_value(const literal_fp_constant_tag c_tag,
+				 const target_float &tf)
+  : constexpr_value(_constness_from_tag(c_tag), tf)
+{}
+
+constexpr_value::constexpr_value(const arithmetic_constant_expr_tag c_tag,
+				 const target_float &tf)
+  : constexpr_value(_constness_from_tag(c_tag), tf)
+{}
+
+constexpr_value::constexpr_value(const target_float &tf)
+  : constexpr_value(_constness_from_tag(), tf)
+{}
+
+constexpr_value::constexpr_value(const address_constant_tag c_tag,
+				 const address_constant &ac)
+  : constexpr_value(_constness_from_tag(c_tag), ac)
+{}
+
+constexpr_value::constexpr_value(const address_constant &ac)
+  : constexpr_value(_constness_from_tag(), ac)
+{}
+
+constexpr_value::~constexpr_value() noexcept
+{
+  switch(_value_kind) {
+  case value_kind::vk_int:
+    _ti.~target_int();
+    break;
+
+  case value_kind::vk_float:
+    _tf.~target_float();
+    break;
+
+  case value_kind::vk_address:
+    _ac.~address_constant();
+    break;
+  }
+}
+
+std::unique_ptr<constexpr_value> constexpr_value::clone() const
+{
+  std::unique_ptr<constexpr_value> c;
+  switch (_value_kind) {
+  case value_kind::vk_int:
+    c.reset(new constexpr_value{_c, _ti});
+    break;
+
+  case value_kind::vk_float:
+    c.reset(new constexpr_value{_c, _tf});
+    break;
+
+  case value_kind::vk_address:
+    c.reset(new constexpr_value{_c, _ac});
+    break;
+  };
+
+  return c;
+}
+
+bool constexpr_value::has_constness(const constness c) const noexcept
+{
+  return _c[_constness_to_size_t(c)];
+}
+
+void constexpr_value::drop_constness() noexcept
+{
+  _c = _constness_from_tag();
+}
+
+void constexpr_value::drop_constness(const constness c) noexcept
+{
+  _c[_constness_to_size_t(c)] = false;
+  if (c == constness::c_arithmetic_constant_expr) {
+    _c[_constness_to_size_t(constness::c_literal_fp_constant)] = false;
+    _c[_constness_to_size_t(constness::c_integer_constant_expr)] = false;
+  }
+}
+
+const target_int& constexpr_value::get_int_value() const noexcept
+{
+  assert(_value_kind == value_kind::vk_int);
+  return _ti;
+}
+
+const target_float& constexpr_value::get_float_value() const noexcept
+{
+  assert(_value_kind == value_kind::vk_float);
+  return _tf;
+}
+
+const constexpr_value::address_constant&
+constexpr_value::get_address_value() const noexcept
+{
+  assert(_value_kind == value_kind::vk_address);
+  return _ac;
+}
+
+constexpr_value::address_constant& constexpr_value::get_address_value() noexcept
+{
+  assert(_value_kind == value_kind::vk_address);
+  return _ac;
+}
+
+bool constexpr_value::is_zero() const noexcept
+{
+  switch(_value_kind) {
+  case value_kind::vk_int:
+    return !_ti;
+
+  case value_kind::vk_float:
+    return _tf.is_zero();
+
+  case value_kind::vk_address:
+    return _ac.is_null();
+  };
+}
+
+constexpr_value::_constness_type constexpr_value::_constness_from_tag() noexcept
+{
+  return _constness_type{};
+}
+
+constexpr_value::_constness_type
+constexpr_value::_constness_from_tag(const integer_constant_expr_tag) noexcept
+{
+  _constness_type c = _constness_from_tag();
+  c[_constness_to_size_t(constness::c_integer_constant_expr)] = true;
+  c[_constness_to_size_t(constness::c_arithmetic_constant_expr)] = true;
+  return c;
+}
+
+constexpr_value::_constness_type
+constexpr_value::_constness_from_tag(const literal_fp_constant_tag) noexcept
+{
+  _constness_type c = _constness_from_tag();
+  c[_constness_to_size_t(constness::c_literal_fp_constant)] = true;
+  c[_constness_to_size_t(constness::c_arithmetic_constant_expr)] = true;
+  return c;
+}
+
+constexpr_value::_constness_type
+constexpr_value::_constness_from_tag(const arithmetic_constant_expr_tag)
+  noexcept
+{
+  _constness_type c = _constness_from_tag();
+  c[_constness_to_size_t(constness::c_arithmetic_constant_expr)] = true;
+  return c;
+}
+
+constexpr_value::_constness_type
+constexpr_value::_constness_from_tag(const address_constant_tag) noexcept
+{
+  _constness_type c = _constness_from_tag();
+  c[_constness_to_size_t(constness::c_address_constant)] = true;
+  return c;
+}
+
+constexpr_value::_constness_type::size_type
+constexpr_value::_constness_to_size_t(const constness c) noexcept
+{
+  return static_cast<constexpr_value::_constness_type::size_type>(c);
+}
---
+++ constexpr_value.hh
@@ -0,0 +1,177 @@
+#ifndef CONSTEXPR_VALUE_HH
+#define CONSTEXPR_VALUE_HH
+
+#include "target_int.hh"
+#include "target_float.hh"
+#include <memory>
+#include <array>
+
+namespace suse
+{
+  namespace cp
+  {
+    namespace ast
+    {
+      class init_declarator;
+      class function_definition;
+      class stmt_labeled;
+      class string_literal;
+      class expr_compound_literal;
+
+      class constexpr_value
+      {
+      public:
+	enum class constness
+	{
+	  c_none,
+	  c_literal_fp_constant,
+	  c_integer_constant_expr,
+	  c_arithmetic_constant_expr,
+	  c_address_constant,
+	  _c_end,
+	};
+
+	enum class value_kind
+	{
+	  vk_int,
+	  vk_float,
+	  vk_address,
+	};
+
+	class address_constant
+	{
+	public:
+	  enum class base_kind
+	  {
+	    bk_constant,
+	    bk_init_declarator,
+	    bk_function_definition,
+	    bk_stmt_labeled,
+	    bk_string_literal,
+	    bk_compound_literal,
+	  };
+
+	  address_constant();
+	  address_constant(const target_int &base);
+	  address_constant(const init_declarator &base,
+			   const bool dereferenced);
+	  address_constant(const function_definition &base,
+			   const bool dereferenced);
+	  address_constant(const stmt_labeled &base,
+			   const bool dereferenced);
+	  address_constant(const string_literal &base,
+			   const bool dereferenced);
+	  address_constant(const expr_compound_literal &base,
+			   const bool dereferenced);
+
+	  bool is_dereferenced() const noexcept
+	  { return _dereferenced; }
+
+	  void undereference() noexcept;
+
+	  void dereference() noexcept;
+
+	  void add_to_offset(mpa::limbs &&ls);
+
+	  void add_to_offset(bool negate, const target_int &i,
+			     const mpa::limbs &j);
+
+	  bool is_null() const noexcept;
+
+	  bool has_same_base_as(const address_constant &ac) const noexcept;
+
+	  const mpa::limbs& get_offset() const noexcept
+	  { return _offset; }
+
+	  base_kind get_base_kind() const noexcept
+	  { return _bk; }
+
+	private:
+	  static mpa::limbs _target_int_to_limbs(const target_int &ti);
+
+	  base_kind _bk;
+	  union {
+	    const init_declarator * const _base_id;
+	    const function_definition * const _base_fd;
+	    const stmt_labeled * const _base_stmt_labeled;
+	    const string_literal * const _base_string_literal;
+	    const expr_compound_literal * const _base_compound_literal;
+	  };
+	  mpa::limbs _offset;
+	  std::size_t _dereferenced;
+	};
+
+	struct integer_constant_expr_tag {};
+	struct literal_fp_constant_tag {};
+	struct arithmetic_constant_expr_tag {};
+	struct address_constant_tag {};
+
+	constexpr_value(const integer_constant_expr_tag c_tag,
+			const target_int &ti);
+	constexpr_value(const arithmetic_constant_expr_tag c_tag,
+			const target_int &ti);
+	constexpr_value(const target_int &ti);
+	constexpr_value(const literal_fp_constant_tag c_tag,
+			const target_float &tf);
+	constexpr_value(const arithmetic_constant_expr_tag c_tag,
+			const target_float &tf);
+	constexpr_value(const target_float &tf);
+	constexpr_value(const address_constant_tag c_tag,
+			const address_constant &ac);
+	constexpr_value(const address_constant &ac);
+	~constexpr_value() noexcept;
+
+	std::unique_ptr<constexpr_value> clone() const;
+
+	value_kind get_value_kind() const noexcept
+	{ return _value_kind; }
+
+	bool has_constness(const constness c) const noexcept;
+
+	void drop_constness() noexcept;
+	void drop_constness(const constness c) noexcept;
+
+	const target_int& get_int_value() const noexcept;
+	const target_float& get_float_value() const noexcept;
+	const address_constant& get_address_value() const noexcept;
+	address_constant& get_address_value() noexcept;
+
+	bool is_zero() const noexcept;
+
+      private:
+	typedef std::array<bool, static_cast<unsigned int>(constness::_c_end)>
+	  _constness_type;
+
+	constexpr_value(const _constness_type &c, const target_int &ti);
+	constexpr_value(const _constness_type &c, const target_float &tf);
+	constexpr_value(const _constness_type &c, const address_constant &ac);
+
+	static _constness_type
+	_constness_from_tag() noexcept;
+	static _constness_type
+	_constness_from_tag(const integer_constant_expr_tag) noexcept;
+	static _constness_type
+	_constness_from_tag(const literal_fp_constant_tag) noexcept;
+	static _constness_type
+	_constness_from_tag(const arithmetic_constant_expr_tag) noexcept;
+	static _constness_type
+	_constness_from_tag(const address_constant_tag) noexcept;
+
+	static _constness_type::size_type
+	_constness_to_size_t(const constness c) noexcept;
+
+	value_kind _value_kind;
+
+	union {
+	  target_int _ti;
+	  target_float _tf;
+	  address_constant _ac;
+	};
+
+	_constness_type _c;
+      };
+    }
+  }
+}
+
+#endif
