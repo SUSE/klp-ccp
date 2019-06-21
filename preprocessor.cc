4a390540eaf955327ef4de3f97a03d90b1a4c72f
Nicolai Stange <nstange@suse.de>
2019-06-21 12:25:20 +0200

    preprocessor: record #undef directives not referring to a prior #define
    Currently, the preprocessor registers macro_undef instances only for
    those directives refererring to an actually defined macro.
    
    When reshuffling output code, it might become possible to reuse an existing
    such #undef from the original code instead of emitting an additional one.
    
    Make the preprocessor unconditionally register all #undef directives.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- preprocessor.cc
+++ preprocessor.cc
@@ -646,10 +646,11 @@ void preprocessor::_handle_pp_directive()
       throw pp_except(remark);
     }
 
+    _pp_result->_add_macro_undef(it_tok->get_value(),
+				 raw_pp_tokens_range{raw_begin, raw_end});
+
     auto it_m = _macros.find(it_tok_id->get_value());
     if (it_m != _macros.end()) {
-      _pp_result->_add_macro_undef(it_tok->get_value(),
-				   raw_pp_tokens_range{raw_begin, raw_end});
       _macros.erase(it_m);
     }
 
