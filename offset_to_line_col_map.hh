74a6c6ba084272f70da87e5104a35f96c6465abf
Nicolai Stange <nstange@suse.de>
2019-08-05 11:31:39 +0200

    Add license notices
    Add license notices to all source files and include a copy of the license
    itself. klp-cpp is currently licensed under GPLv2 only.
    
    For the files copied from the GCC testsuite (which are under GPLv3), make
    add a notice in the resp. directories each.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- offset_to_line_col_map.hh
+++ offset_to_line_col_map.hh
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2019  SUSE Software Solutions Germany GmbH
+ *
+ * This file is part of klp-ccp.
+ *
+ * klp-ccp is free software: you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * klp-ccp is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with klp-ccp. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef OFFSET_TO_LINE_COL_MAP_HH
 #define OFFSET_TO_LINE_COL_MAP_HH
 
