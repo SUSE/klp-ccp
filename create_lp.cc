SUSE/klp-ccp

commit 1e132ddada8222cbe1c019aeaa0a6ac05b470dbb

cerate_lp: don't attempt to externalize objects with no linkage
The Linux kernel has the following declaration for arch/x86:

  register unsigned long current_stack_pointer asm(_ASM_SP);

current_stack_pointer has no linkage and cannot get externalized. Don't
confuse the lp_creation_policy by attempting to do so.

Signed-off-by: Nicolai Stange <nstange@suse.de>

create_lp.cc
@@ -4831,20 +4831,27 @@ void _closure_builder::_construct_fd_id_closure()
       = [&](dep_on_obj &d) {
 	  object_info &oi = d.get_object_info();
 	  if (!oi.shall_externalize_valid) {
-	    oi.shall_externalize =
-	      (_pol.shall_externalize_object(d.oidi.get().init_declarator,
-					     _remarks));
-	    oi.shall_externalize_valid = true;
+	    const ast::linkage &l = d.oidi.get().init_declarator.get_linkage();
+	    if (l.get_linkage_kind() != ast::linkage::linkage_kind::none) {
+	      oi.shall_externalize =
+		(_pol.shall_externalize_object(d.oidi.get().init_declarator,
+					       _remarks));
+	      oi.shall_externalize_valid = true;
 
-	    if (!oi.shall_externalize) {
-	      const pp_tokens &toks = _ai.atu.get_pp_result().get_pp_tokens();
-	      const std::string &name =
-		toks[d.from_eid.get_id_tok()].get_value();
-	      code_remark remark
-		(code_remark::severity::warning,
-		 "referenced object \"" + name + "\" cannot get externalized",
-		 _ai.atu.get_pp_result(), d.from_eid.get_tokens_range());
-	      _remarks.add(remark);
+	      if (!oi.shall_externalize) {
+		const pp_tokens &toks = _ai.atu.get_pp_result().get_pp_tokens();
+		const std::string &name =
+		  toks[d.from_eid.get_id_tok()].get_value();
+		code_remark remark
+		  (code_remark::severity::warning,
+		   "referenced object \"" + name + "\" cannot get externalized",
+		   _ai.atu.get_pp_result(), d.from_eid.get_tokens_range());
+		_remarks.add(remark);
+	      }
+	    } else {
+	      // File scope declaration with 'register' storage class specifier.
+	      oi.shall_externalize = false;
+	      oi.shall_externalize_valid = true;
 	    }
 	  }
 

commit a98f673d4329d5f1778173ff0d90d501a1e39c56

create_lp: don't skip externalization of 'static' local objects
Commit 1e132ddada82 ("cerate_lp: don't attempt to externalize objects with
no linkage") attempted to supress externalization attempts of global
variables declared with the "register" storage class by filtering any
objects without linkage.

However, this check is too broad, because local variables declared "static"
have no linkage either, yet should be subject to externalization.

Fix this by checking explicitly for the "register" storage class specifier
rather than for non-linkage.

Fixes: commit 1e132ddada82 ("cerate_lp: don't attempt to externalize
       objects with no linkage")
Signed-off-by: Nicolai Stange <nstange@suse.de>

create_lp.cc
@@ -4834,8 +4834,11 @@ void _closure_builder::_construct_fd_id_closure()
       = [&](dep_on_obj &d) {
 	  object_info &oi = d.get_object_info();
 	  if (!oi.shall_externalize_valid) {
+	    const ast::storage_class_specifier * const scs =
+	      d.oidi.get().enclosing_declaration.storage_class_specifier;
 	    const ast::linkage &l = d.oidi.get().init_declarator.get_linkage();
-	    if (l.get_linkage_kind() != ast::linkage::linkage_kind::none) {
+	    if (!scs ||
+		(scs->get_storage_class() != ast::storage_class::sc_register)) {
 	      oi.shall_externalize =
 		(_pol.shall_externalize_object(d.oidi.get().init_declarator,
 					       _remarks));