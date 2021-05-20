SUSE/klp-ccp

commit e2d7e3e36fb4f8136d7a830f5521c296b39059ea

create_lp: track unevaluated subexprs for cond op + __builtin_choose_expr()
Determine and track unevaluated subexpressions of the ternary operator as
well as __builtin_choose_expr() so that functions and objects referenced
therefrom won't get added to the closure.

Signed-off-by: Nicolai Stange <nstange@suse.de>

create_lp.cc
@@ -3247,13 +3247,13 @@ namespace
     void _handle_expr(const ast::expr_member_deref &e);
     void _handle_expr(const ast::expr_cast &e);
     void _handle_expr(const ast::expr_compound_literal &e);
-    void _handle_expr(const ast::expr_func_invocation &e);
+    bool _handle_expr(const ast::expr_func_invocation &e);
     void _handle_expr(const ast::expr_unop_pre &e);
     void _handle_expr(const ast::expr_unop_post &e);
     void _handle_expr(const ast::expr_binop &e);
     void _handle_expr(const ast::expr_assignment &e);
     void _handle_expr(const ast::expr_array_subscript &e);
-    void _handle_expr(const ast::expr_conditional &e);
+    bool _handle_expr(const ast::expr_conditional &e);
     void _handle_expr(const ast::expr_id &e);
 
     void _handle_stmt(const ast::stmt_expr &s);
@@ -3277,6 +3277,15 @@ namespace
     void _push_unevaluated(const ast::_ast_entity &e);
     void _pop_unevaluated();
     bool _is_evaluated(const ast::expr &e) const noexcept;
+
+    enum class _constexpr_bool_value
+    {
+      unknown,
+      nonzero,
+      zero
+    };
+
+    static _constexpr_bool_value _expr_to_bool(const ast::expr &e) noexcept;
 
     static bool _is_non_local_linkage(const ast::linkage &l) noexcept;
 
@@ -3544,8 +3553,7 @@ void _ast_info_collector::operator()()
 	return false;
       },
       [this](const ast::expr_func_invocation &e) {
-	_handle_expr(e);
-	return false;
+	return _handle_expr(e);
       },
       [this](const ast::expr_unop_pre &e) {
 	_handle_expr(e);
@@ -3568,8 +3576,7 @@ void _ast_info_collector::operator()()
 	return false;
       },
       [this](const ast::expr_conditional &e) {
-	_handle_expr(e);
-	return false;
+	return _handle_expr(e);
       },
       [this](const ast::expr_id &e) {
 	_handle_expr(e);
@@ -3705,6 +3712,12 @@ void _ast_info_collector::operator()()
       [this](const ast::expr_alignof_expr&) {
 	_pop_unevaluated();
       },
+      [this](const ast::expr_func_invocation&) {
+	_pop_unevaluated();
+      },
+      [this](const ast::expr_conditional&) {
+	_pop_unevaluated();
+      },
       [&](const ast::struct_or_union_def&) {
 	_leave_souoed();
       },
@@ -3766,6 +3779,8 @@ void _ast_info_collector::operator()()
 	      const ast::typeof_expr,
 	      const ast::expr_sizeof_expr,
 	      const ast::expr_alignof_expr,
+	      const ast::expr_func_invocation,
+	      const ast::expr_conditional,
 	      const ast::struct_or_union_def,
 	      const ast::enum_def>>
     (std::move(pre), std::move(post));
@@ -4079,19 +4094,45 @@ void _ast_info_collector::_handle_expr(const ast::expr_compound_literal &e)
   _require_complete_type(*e.get_type_name().get_type());
 }
 
-void _ast_info_collector::_handle_expr(const ast::expr_func_invocation &e)
+bool _ast_info_collector::_handle_expr(const ast::expr_func_invocation &e)
 {
   // For a function invocation, the compiler will have to set things
   // up on the stack etc. and the return types as well as the
   // parameter types must all be complete.
-  types::handle_types<void>
-    ((wrap_callables<default_action_unreachable<void, type_set<> >::type>
+  return types::handle_types<bool>
+    ((wrap_callables<default_action_unreachable<bool, type_set<> >::type>
       ([&](const types::pointer_type &pt) {
 	 _require_complete_function_ret_and_param_types
 					(*pt.get_pointed_to_type());
+	 return false;
        },
-       [](const types::builtin_func_type&) {
-	 // Nothing required.
+       [&](const types::builtin_func_type &bft) {
+	 // See whether this is a __builtin_choose_expr() and
+	 // add the non-taken subexpression to the stack of unevaluated
+	 // expressions.
+	 if(!(builtins::impl::builtin_func_choose_expr::is_factory
+	      (bft.get_builtin_func_factory()))) {
+	   return false;
+	 }
+
+	 const ast::expr_list * const el = e.get_args();
+	 assert(el);
+	 assert(el->size() == 3);
+	 switch (_expr_to_bool((*el)[0])) {
+	 case _constexpr_bool_value::unknown:
+	   assert(0);
+	   return false;
+
+	 case _constexpr_bool_value::nonzero:
+	   _push_unevaluated((*el)[2]);
+	   break;
+
+	 case _constexpr_bool_value::zero:
+	   _push_unevaluated((*el)[1]);
+	   break;
+	 };
+
+	 return true;
        })),
      *e.get_func().get_type());
 }
@@ -4172,12 +4213,29 @@ void _ast_info_collector::_handle_expr(const ast::expr_array_subscript &e)
   _require_complete_pointed_to_type(*e.get_index().get_type());
 }
 
-void _ast_info_collector::_handle_expr(const ast::expr_conditional &e)
+bool _ast_info_collector::_handle_expr(const ast::expr_conditional &e)
 {
   _require_complete_type(*e.get_cond().get_type());
   if (e.get_expr_true())
     _require_complete_type(*e.get_expr_true()->get_type());
   _require_complete_type(*e.get_expr_false().get_type());
+
+  switch (_expr_to_bool(e.get_cond())) {
+  case _constexpr_bool_value::unknown:
+    return false;
+
+  case _constexpr_bool_value::nonzero:
+    _push_unevaluated(e.get_expr_false());
+    return true;
+
+  case _constexpr_bool_value::zero:
+    if (e.get_expr_true()) {
+      _push_unevaluated(*e.get_expr_true());
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 void _ast_info_collector::_handle_expr(const ast::expr_id &e)
@@ -4550,6 +4608,18 @@ bool _ast_info_collector::_is_evaluated(const ast::expr &e) const noexcept
   }
 
   return true;
+}
+
+_ast_info_collector::_constexpr_bool_value
+_ast_info_collector::_expr_to_bool(const ast::expr &e) noexcept
+{
+  if (!e.is_constexpr())
+    return _constexpr_bool_value::unknown;
+
+  if (e.get_constexpr_value().is_zero())
+    return _constexpr_bool_value::zero;
+  else
+    return _constexpr_bool_value::nonzero;
 }
 
 bool _ast_info_collector::_is_non_local_linkage(const ast::linkage &l) noexcept

testsuite/ccp/builtin-choose-expr-1.c.expected
@@ -1,6 +1,5 @@
 /* klp-ccp: from testsuite/ccp/builtin-choose-expr-1.c */
-static void g(void)
-{}
+void g(void);
 
 static void h(void)
 {}

testsuite/ccp/unevaluated-2.c
@@ -1,0 +1,47 @@
+static int g(void)
+{
+	return 1;
+}
+
+static int h(void)
+{
+	return 1;
+}
+
+static int i(void)
+{
+	return 1;
+}
+
+static int j(void)
+{
+	return 1;
+}
+
+static int k(void)
+{
+	return 1;
+}
+
+static int l(void)
+{
+	return 1;
+}
+
+static int m(void)
+{
+	return 1;
+}
+
+static int n(void)
+{
+	return 1;
+}
+
+void pu_f(int x)
+{
+	x ? g() : h();
+	0 ? i() : j();
+	1 ? k() : l();
+	(m(), 0) ? : n();
+}

testsuite/ccp/unevaluated-2.c.expected
@@ -1,0 +1,42 @@
+/* klp-ccp: from testsuite/ccp/unevaluated-2.c */
+static int g(void)
+{
+	return 1;
+}
+
+static int h(void)
+{
+	return 1;
+}
+
+static int i(void);
+
+static int j(void)
+{
+	return 1;
+}
+
+static int k(void)
+{
+	return 1;
+}
+
+static int l(void);
+
+static int m(void)
+{
+	return 1;
+}
+
+static int n(void)
+{
+	return 1;
+}
+
+void klpp_pu_f(int x)
+{
+	x ? g() : h();
+	0 ? i() : j();
+	1 ? k() : l();
+	(m(), 0) ? : n();
+}

testsuite/ccp/unevaluated-3.c
@@ -1,0 +1,35 @@
+static int g(void)
+{
+	return 1;
+}
+
+static int h(void)
+{
+	return 1;
+}
+
+static int i(void)
+{
+	return 1;
+}
+
+static int j(void)
+{
+	return 1;
+}
+
+static int k(void)
+{
+	return 1;
+}
+
+static int l(void)
+{
+	return 1;
+}
+
+void pu_f(void)
+{
+	__builtin_choose_expr(0, i(), j());
+	__builtin_choose_expr(1, k(), l());
+}

testsuite/ccp/unevaluated-3.c.expected
@@ -1,0 +1,20 @@
+/* klp-ccp: from testsuite/ccp/unevaluated-3.c */
+static int i(void);
+
+static int j(void)
+{
+	return 1;
+}
+
+static int k(void)
+{
+	return 1;
+}
+
+static int l(void);
+
+void klpp_pu_f(void)
+{
+	__builtin_choose_expr(0, i(), j());
+	__builtin_choose_expr(1, k(), l());
+}

commit 7f976b495809a2ae9f7017d2337557847d35e6c3

create_lp: don't register conditional, unevaluated subexpression as unevaluated
create_lp's class _ast_info_collector maintains an internal stack of
expressions known to not ever get evaluated during runime and _is_evaluated()
compares a given expression and all its parents against the top of that stack.

During AST traversal, the DFS pre visitor examines constant expressions
and registers subexpression of the ternary operator, __builtin_choose_expr()
and the logical || resp && operators as unevaluated as appropriate.

However, if the examined parent expression has itself been registered as
unevaluated, this will effectively make _is_evaluated() wrongly return true
for the other sibling subexpression: _is_evaluated() would compare it as well
as its parent only against the top of the stack and fail to find a match.

Fix this by pushing subexpressions of the ternary operator,
__builtin_choose_expr() and the logical || resp && operators on the unevaluated
stack only if _is_evaluated() returns true the examined parent expression.

Fixes: e2d7e3e36fb4 ("create_lp: track unevaluated subexprs for cond op +
                      __builtin_choose_expr()")
Fixes: ded3c431ba58 ("create_lp: track unevaluated subexprs of logical binops")
Signed-off-by: Nicolai Stange <nstange@suse.de>

create_lp.cc
@@ -4125,6 +4125,9 @@ bool _ast_info_collector::_handle_expr(const ast::expr_func_invocation &e)
 	   return false;
 	 }
 
+	 if (!_is_evaluated(e))
+	   return false;
+
 	 const ast::expr_list * const el = e.get_args();
 	 assert(el);
 	 assert(el->size() == 3);
@@ -4202,6 +4205,9 @@ bool _ast_info_collector::_handle_expr(const ast::expr_binop &e)
     break;
 
   case ast::binary_op::logical_and:
+    if (!_is_evaluated(e))
+      return false;
+
     if (_expr_to_bool(e.get_left_expr()) == _constexpr_bool_value::zero) {
       _push_unevaluated(e.get_right_expr());
       return true;
@@ -4209,6 +4215,9 @@ bool _ast_info_collector::_handle_expr(const ast::expr_binop &e)
     break;
 
   case ast::binary_op::logical_or:
+    if (!_is_evaluated(e))
+      return false;
+
     if (_expr_to_bool(e.get_left_expr()) == _constexpr_bool_value::nonzero) {
       _push_unevaluated(e.get_right_expr());
       return true;
@@ -4245,6 +4254,9 @@ bool _ast_info_collector::_handle_expr(const ast::expr_conditional &e)
   if (e.get_expr_true())
     _require_complete_type(*e.get_expr_true()->get_type());
   _require_complete_type(*e.get_expr_false().get_type());
+
+  if (!_is_evaluated(e))
+    return false;
 
   switch (_expr_to_bool(e.get_cond())) {
   case _constexpr_bool_value::unknown: