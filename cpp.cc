01900feed75de68630681cfb894d25d274c87a25
Nicolai Stange <nstange@suse.de>
2021-06-16 12:31:48 +0200

    cpp: implement support for #line annotations
    When debugging klp-ccp setups, it's helpful being able to figure out which
    headers got included from where. Implement support to for emitting #line
    annotations in cpp, enabled upon user request via the new --annotate/-a
    option.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- cpp.cc
+++ cpp.cc
@@ -31,11 +31,12 @@ using namespace klp::ccp;
 
 static const char prog_name[] = "klp-ccp";
 
-static const char optstr[] = ":hc:";
+static const char optstr[] = ":hc:a";
 
 static const option longopts[] {
 	{ "help", 0, nullptr, 'h' },
 	{ "compiler", 1, nullptr, 'c' },
+	{ "annotate", 1, nullptr, 'a' },
 	{ nullptr, 0, nullptr, 0 }
 };
 
@@ -55,7 +56,11 @@ static void show_help(const char * const prog)
   std::cout << "Options:" << std::endl;
   std::cout
     << " -c, --compiler=ARCH-COMPILER-VERSION"
-    << "\t\tTarget compiler specification." << std::endl
+    << "\t\tTarget compiler specification."
+    << std::endl;
+  std::cout
+    << " -a, --annotate"
+    << "\t\t\t\t\tEmit source location annotations." << std::endl
     << std::endl;
 
   std::cout
@@ -93,6 +98,7 @@ int main(int argc, char* argv[])
 {
   bool do_help = false;
   char *o_compiler = nullptr;
+  bool annotate = false;
   int o;
   int longindex = -1;
   while (optind < argc && std::strcmp(argv[optind], "--") &&
@@ -108,6 +114,10 @@ int main(int argc, char* argv[])
 	return 1;
       }
       o_compiler = optarg;
+      break;
+
+    case 'a':
+      annotate = true;
       break;
 
     case '?':
@@ -195,10 +205,11 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  const pp_tokens &tokens = pp.get_result().get_pp_tokens();
+  const auto &ppr = pp.get_result();
+  const pp_tokens &tokens = ppr.get_pp_tokens();
   while(true) {
     try {
-      auto &tok = tokens[pp.read_next_token()];
+      const auto &tok = tokens[pp.read_next_token()];
       if (!pp.get_remarks().empty()) {
 	std::cerr << pp.get_remarks();
 	pp.get_remarks().clear();
@@ -206,8 +217,6 @@ int main(int argc, char* argv[])
 
       if (tok.is_eof())
 	break;
-
-      std::cout << tok.stringify();
 
     } catch (const pp_except&) {
 	std::cerr << pp.get_remarks();
@@ -218,6 +227,72 @@ int main(int argc, char* argv[])
     } catch (const semantic_except&) {
       std::cerr << pp.get_remarks();
       return 3;
+    }
+  }
+
+  const auto &raw_tokens = ppr.get_raw_tokens();
+  const auto &emit_src_annotation =
+    [&](const pp_result::header_inclusion_node &h,
+	const raw_pp_token_index index_raw) {
+      const auto &r = h.get_range();
+      assert((index_raw == r.begin && r.begin == r.end)
+	     || index_raw < raw_tokens.size());
+      assert((raw_pp_tokens_range{index_raw,
+				 r.begin == r.end ? index_raw : index_raw + 1}
+	      .is_contained_in(r)));
+      const auto offset_in_file =
+	r.begin != r.end ? raw_tokens[index_raw].get_range_in_file().begin : 0;
+      const auto line = h.offset_to_line_col(offset_in_file).first;
+
+      if (!annotate)
+	return;
+
+      std::cout << "#line " << line << " \"" << h.get_filename() << '"'
+		<< std::endl;
+    };
+
+  pp_token_index tok_index = 0;
+  raw_pp_token_index raw_tok_index = 0;
+  auto emit_tokens_until =
+    [&](const raw_pp_token_index end) {
+      for (; tok_index < tokens.size(); ++tok_index) {
+	const auto &tok = tokens[tok_index];
+	if (tok.get_token_source().begin >= end)
+	  break;
+	std::cout << tok.stringify();
+      }
+    };
+
+  auto it_src =
+    ppr.intersecting_sources_begin(raw_pp_tokens_range{0, raw_tokens.size()});
+  auto it_src_end =
+    ppr.intersecting_sources_end(raw_pp_tokens_range{0, raw_tokens.size()});
+  if (it_src == it_src_end)
+    return 0;
+
+  while (true) {
+    const pp_result::header_inclusion_node &h = *it_src;
+    ++it_src;
+
+    const auto &cur_range = h.get_range();
+    if (it_src == it_src_end) {
+      emit_src_annotation(h, raw_tok_index);
+      emit_tokens_until(cur_range.end);
+      break;
+
+    }
+
+    const auto &next_range = it_src->get_range();
+    if (next_range.is_contained_in(cur_range)) {
+      emit_src_annotation(h, raw_tok_index);
+      emit_tokens_until(next_range.begin);
+      raw_tok_index = next_range.begin;
+    } else {
+      assert(cur_range.is_contained_in(next_range) ||
+	     cur_range.end == next_range.begin);
+      emit_src_annotation(h, raw_tok_index);
+      emit_tokens_until(cur_range.end);
+      raw_tok_index = cur_range.end;
     }
   }
 
