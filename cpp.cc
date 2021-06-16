0171363c3a3859bcf551e5d7c2743fb2a54815f7
Nicolai Stange <nstange@suse.de>
2021-06-16 07:13:30 +0200

    cpp: allow for specification of compiler to emulate + its invocation
    Currently, the cpp tool is used only from the testsuite and the compiler
    to emulate is hardcoded as "x86_64-gcc-4.8.5". Moreover, it is not
    possible to specify the compiler invocation.
    
    In order to make the cpp tool more useful for debugging, extend its
    command line to allow for the specification of the compiler to emulate as
    well as its invocation, just in analogy to the klp-ccp tool.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- cpp.cc
+++ cpp.cc
@@ -16,6 +16,9 @@
  * along with klp-ccp. If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <unistd.h>
+#include <getopt.h>
+#include <cstring>
 #include <iostream>
 #include "preprocessor.hh"
 #include "cmdline_except.hh"
@@ -26,35 +29,179 @@
 
 using namespace klp::ccp;
 
+static const char prog_name[] = "klp-ccp";
+
+static const char optstr[] = ":hc:";
+
+static const option longopts[] {
+	{ "help", 0, nullptr, 'h' },
+	{ "compiler", 1, nullptr, 'c' },
+	{ nullptr, 0, nullptr, 0 }
+};
+
+static void show_usage(const char * const prog)
+{
+  std::cerr << "See '" << prog << " --help' for usage." << std::endl;
+}
+
+static void show_help(const char * const prog)
+{
+  std::cout
+    << "Usage: " << prog
+    << " OPTIONS"
+    << " -- ORIGINAL COMPILER COMMAND LINE"
+    << std::endl;
+
+  std::cout << "Options:" << std::endl;
+  std::cout
+    << " -c, --compiler=ARCH-COMPILER-VERSION"
+    << "\t\tTarget compiler specification." << std::endl
+    << std::endl;
+
+  std::cout
+    << "ORIGINAL COMPILER COMMAND LINE"
+    << "\tdenotes the compiler invocation from the live" << std::endl
+    << "\t\t\t\tpatching target's original compilation, without" << std::endl
+    << "\t\t\t\tthe program name."
+    << std::endl << std::endl;
+  std::cout
+    << "For further information, please see"
+    << " <https://github.com/suse/klp-ccp>."
+    << std::endl;
+}
+
+static void show_opt_duplicate(const char * const prog,
+			       const char * const o)
+{
+  std::cerr << "command line error: "
+	    << " option '" << o << "' specified more than once"
+	    << std::endl;
+  show_usage(prog);
+}
+
+static void show_opt_missing(const char * const prog,
+			       const char * const o)
+{
+  std::cerr << "command line error: "
+	    << " option '" << o << "' is required"
+	    << std::endl;
+  show_usage(prog);
+}
+
+
 int main(int argc, char* argv[])
 {
-  if (argc != 2) {
-    std::cerr << "error: usage: " << argv[0] << " <file>" << std::endl;
+  bool do_help = false;
+  char *o_compiler = nullptr;
+  int o;
+  int longindex = -1;
+  while (optind < argc && std::strcmp(argv[optind], "--") &&
+	 (o = getopt_long(argc, argv, optstr, longopts, &longindex)) != -1) {
+    switch (o) {
+    case 'h':
+      do_help = true;
+      break;
+
+    case 'c':
+      if (o_compiler) {
+	show_opt_duplicate(prog_name, "--compiler");
+	return 1;
+      }
+      o_compiler = optarg;
+      break;
+
+    case '?':
+      std::cerr << "command line error: invalid option '";
+      if (optopt)
+	std::cerr << '-' << static_cast<char>(optopt);
+      else
+	std::cerr << argv[optind - 1];
+      std::cerr << '\'' << std::endl;
+      show_usage(prog_name);
+      return 1;
+
+    case ':':
+      std::cerr << "command line error: missing parameter for option '";
+      if (optopt)
+	std::cerr << '-' << static_cast<char>(optopt);
+      else
+	std::cerr << longopts[longindex].name;
+      std::cerr << '\'' << std::endl;
+      show_usage(prog_name);
+      return 1;
+    }
+  }
+
+  if (do_help) {
+    show_help(prog_name);
+    return 0;
+  }
+
+  if (optind != argc && !std::strcmp(argv[optind], "--"))
+    ++optind;
+
+  if (optind == argc) {
+    std::cerr << "command line error: missing compiler invocation" << std::endl;
+    show_usage(prog_name);
+    return 1;
+  }
+
+  if (!o_compiler) {
+    show_opt_missing(prog_name, "--compiler");
+    return 1;
+  }
+
+  char *o_compiler_version = std::strrchr(o_compiler, '-');
+  if (!o_compiler_version || !std::strlen(o_compiler_version + 1)) {
+    std::cerr
+      << "command line error: compiler version specification missing in '"
+      << o_compiler << '\'' << std::endl;
+    show_usage(prog_name);
+    return 1;
+  }
+  *o_compiler_version = '\0';
+  ++o_compiler_version;
+
+  std::unique_ptr<target> tgt;
+  if (!strcmp(o_compiler, "x86_64-gcc")) {
+    try {
+      tgt.reset(new target_x86_64_gcc{o_compiler_version});
+    } catch (const cmdline_except &e) {
+      std::cerr << "command line error: " << e.what() << std::endl;
+      show_usage(prog_name);
+      return 1;
+    }
+  } else {
+    std::cerr << "command line error: "
+	      << "unrecognized compiler specification '"
+	      << o_compiler << '\'' << std::endl;
+    show_usage(prog_name);
     return 1;
   }
 
   header_resolver hr;
-  target_x86_64_gcc tgt{"4.8.5"};
-  preprocessor p{hr, tgt};
+  preprocessor pp{hr, *tgt};
   try {
-    tgt.parse_command_line(1, const_cast<const char**>(&argv[1]), hr, p,
-			   [](const std::string&) {
-			     assert(0);
-			     __builtin_unreachable();
-			   });
+    tgt->parse_command_line(argc - optind,
+			    const_cast<const char **>(&argv[optind]),
+			    hr, pp,
+			    [&](const std::string &warning) {
+			      std::cerr << "compiler command line warning: "
+					<< warning << std::endl;
+			    });
   } catch (const cmdline_except &e) {
-    std::cerr << "internal compiler command line error: "
+    std::cerr << "compiler command line error: "
 	      << e.what() << std::endl;
     return 1;
   }
 
-  const pp_tokens &tokens = p.get_result().get_pp_tokens();
+  const pp_tokens &tokens = pp.get_result().get_pp_tokens();
   while(true) {
     try {
-      auto &tok = tokens[p.read_next_token()];
-      if (!p.get_remarks().empty()) {
-	std::cerr << p.get_remarks();
-	p.get_remarks().clear();
+      auto &tok = tokens[pp.read_next_token()];
+      if (!pp.get_remarks().empty()) {
+	std::cerr << pp.get_remarks();
+	pp.get_remarks().clear();
       }
 
       if (tok.is_eof())
@@ -63,13 +210,13 @@ int main(int argc, char* argv[])
       std::cout << tok.stringify();
 
     } catch (const pp_except&) {
-	std::cerr << p.get_remarks();
+	std::cerr << pp.get_remarks();
 	return 1;
     } catch (const parse_except&) {
-      std::cerr << p.get_remarks();
+      std::cerr << pp.get_remarks();
       return 2;
     } catch (const semantic_except&) {
-      std::cerr << p.get_remarks();
+      std::cerr << pp.get_remarks();
       return 3;
     }
   }
