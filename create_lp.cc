SUSE/klp-ccp

commit 7b846ebf16df201e134c82d35e5241da00ef22b7

create_lp: implement support for C11 _Generic()s
Implement C11 _Generic() support to create_lp. In particular, make the
ast_info_collector record dependencies on complete types as well make it track
the unevaluated subexpressions for the purpose of disabling externalizations
for any symbols referenced from those.

Signed-off-by: Nicolai Stange <nstange@suse.de>

create_lp.cc
@@ -3246,6 +3246,7 @@ namespace
     bool _handle_expr(const ast::expr_alignof_expr &e);
     void _handle_expr(const ast::expr_sizeof_type_name &e);
     void _handle_expr(const ast::expr_alignof_type_name &e);
+    bool _handle_expr(const ast::expr_generic &e);
     bool _handle_expr(const ast::expr_builtin_choose_expr &e);
     void _handle_expr(const ast::expr_builtin_offsetof &e);
     void _handle_expr(const ast::expr_member_deref &e);
@@ -3545,6 +3546,9 @@ void _ast_info_collector::operator()()
 	_handle_expr(e);
 	return false;
       },
+      [this](const ast::expr_generic &e) {
+	return _handle_expr(e);
+      },
       [this](const ast::expr_builtin_choose_expr &e) {
 	return _handle_expr(e);
       },
@@ -3719,6 +3723,9 @@ void _ast_info_collector::operator()()
       [this](const ast::expr_alignof_expr&) {
 	_pop_unevaluated();
       },
+      [this](const ast::expr_generic&) {
+	_pop_unevaluated();
+      },
       [this](const ast::expr_builtin_choose_expr&) {
 	_pop_unevaluated();
       },
@@ -3762,6 +3769,7 @@ void _ast_info_collector::operator()()
 	      const ast::expr_alignof_expr,
 	      const ast::expr_sizeof_type_name,
 	      const ast::expr_alignof_type_name,
+	      const ast::expr_generic,
 	      const ast::expr_builtin_choose_expr,
 	      const ast::expr_builtin_offsetof,
 	      const ast::expr_member_deref,
@@ -3800,6 +3808,7 @@ void _ast_info_collector::operator()()
 	      const ast::typeof_expr,
 	      const ast::expr_sizeof_expr,
 	      const ast::expr_alignof_expr,
+	      const ast::expr_generic,
 	      const ast::expr_builtin_choose_expr,
 	      const ast::expr_binop,
 	      const ast::expr_conditional,
@@ -4087,6 +4096,34 @@ void _ast_info_collector::_handle_expr(const ast::expr_sizeof_type_name &e)
 void _ast_info_collector::_handle_expr(const ast::expr_alignof_type_name &e)
 {
   _require_complete_type(*e.get_type_name().get_type());
+}
+
+bool _ast_info_collector::_handle_expr(const ast::expr_generic &e)
+{
+  _require_complete_type(*e.get_control_expr().get_type());
+
+  if (!_is_evaluated(e))
+    return false;
+
+  _unevaluated_stack_entry unevaluated = { std::cref(e.get_control_expr()) };
+  const ast::expr &match = e.get_match();
+  e.get_association_list().for_each
+    ([&](const ast::generic_association &ga) {
+      const ast::type_name * const tn = ga.get_type_name();
+
+      if (tn)
+	_require_complete_type(*tn->get_type());
+
+      const ast::expr &e = ga.get_expr();
+      if (&e != &match)
+	unevaluated.push_back(std::cref(e));
+    });
+
+  if (unevaluated.empty())
+    return false;
+
+  _push_unevaluated(std::move(unevaluated));
+  return true;
 }
 
 bool _ast_info_collector::_handle_expr(const ast::expr_builtin_choose_expr &e)

ast.cc
@@ -887,7 +887,8 @@ bool generic_association_list::_process(const_processor<bool> &p) const
 expr_generic::expr_generic(const pp_tokens_range &tr,
 			   expr *&&ctrl_e,
 			   generic_association_list *&&gal) noexcept
-  : expr(tr), _ctrl_e(*mv_p(std::move(ctrl_e))), _gal(*mv_p(std::move(gal)))
+  : expr(tr), _ctrl_e(*mv_p(std::move(ctrl_e))), _gal(*mv_p(std::move(gal))),
+    _match(nullptr)
 {
   _ctrl_e._set_parent(*this);
   _gal._set_parent(*this);
@@ -897,6 +898,12 @@ expr_generic::~expr_generic() noexcept
 {
   delete &_ctrl_e;
   delete &_gal;
+}
+
+const expr& expr_generic::get_match() const noexcept
+{
+  assert(_match);
+  return *_match;
 }
 
 _ast_entity* expr_generic::_get_child(const size_t i) const noexcept

ast.hh
@@ -939,6 +939,14 @@ namespace klp
 
 	virtual void evaluate_type(ast &a, const target &tgt) override;
 
+	const expr& get_control_expr() const noexcept
+	{ return _ctrl_e; }
+
+	const generic_association_list& get_association_list() const noexcept
+	{ return _gal; }
+
+	const expr& get_match() const noexcept;
+
       private:
 	virtual _ast_entity* _get_child(const size_t i) const noexcept override;
 
@@ -949,6 +957,7 @@ namespace klp
 
 	expr &_ctrl_e;
 	generic_association_list &_gal;
+	const expr *_match;
       };
 
       class expr_builtin_choose_expr final : public expr

ast_evaluate.cc
@@ -5620,7 +5620,7 @@ void expr_generic::evaluate_type(ast &a, const target &tgt)
 
 
   const expr &e = match->get_expr();
-
+  _match = &e;
   if (e.is_constexpr() || e.is_dereferenced_const_addr())
     _set_value(e.get_constexpr_value().clone());
   _set_type(e.get_type());

commit c4bf274a3958d5c23d73c1146508bb8e1d8d6c14

create_lp: fix compilation errors due to implicit reference_wrapper conversions
Commit 7b846ebf16 ("create_lp: implement support for C11 _Generic()s")
introduced two usages of std::cref() on an ast::expr each, where the result gets
later assigned to std::reference_wrapper<ast::_ast_entity>. This works fine
with a recent libstdc++, as ast::expr is derived from ast::_ast_entity and
there seem to be implicit conversions available between the corresponding
reference_wrapper<> types. With earlier libstdc++ implementations this is
apparently not the case and compilation errors out. Fix this by explicitly
specifying ast::_ast_entity for std::cref()'s template parameter.

Fixes: 7b846ebf16 ("create_lp: implement support for C11 _Generic()s")
Signed-off-by: Nicolai Stange <nstange@suse.de>

create_lp.cc
@@ -4122,7 +4122,9 @@ bool _ast_info_collector::_handle_expr(const ast::expr_generic &e)
   if (!_is_evaluated(e))
     return false;
 
-  _unevaluated_stack_entry unevaluated = { std::cref(e.get_control_expr()) };
+  _unevaluated_stack_entry unevaluated = {
+    std::cref<ast::_ast_entity>(e.get_control_expr())
+  };
   const ast::expr &match = e.get_match();
   e.get_association_list().for_each
     ([&](const ast::generic_association &ga) {
@@ -4133,7 +4135,7 @@ bool _ast_info_collector::_handle_expr(const ast::expr_generic &e)
 
       const ast::expr &e = ga.get_expr();
       if (&e != &match)
-	unevaluated.push_back(std::cref(e));
+	unevaluated.push_back(std::cref<ast::_ast_entity>(e));
     });
 
   if (unevaluated.empty())